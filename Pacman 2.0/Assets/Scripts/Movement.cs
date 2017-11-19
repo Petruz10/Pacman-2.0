9d79c44511808c122a7fd16c4281a37fd0edacad
Petruz <petrairisl36@gmail.com>
2017-11-18 17:20:09 -0800

    Movement somewhat working

---
+++ Pacman 2.0/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+	public float speed;
+	private Vector3 playerPos = new Vector3 (0, 0, 0);
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	//Reused some script from the BrickBuster game
+	// Update is called once per frame
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * speed);
+		float yPos = transform.position.y + (Input.GetAxis("Vertical") * speed);
+
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -40f, 40f),(yPos), 0f);
+		transform.position = playerPos;
+	}
+}
